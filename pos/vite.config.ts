756a6351d46ef153f0ebb6e855c7aab9abcef24f
vishalsvnix <vishal@svnix.solutions>
2025-04-29 16:36:21 +0530

    Update vite.config.ts

--- pos/vite.config.ts
+++ pos/vite.config.ts
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
 	server: {
-		port: 8081,
+		port: 8080,
 		host: '0.0.0.0',
 		proxy: proxyOptions
 	},
